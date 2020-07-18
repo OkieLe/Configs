e4dd6f2a7fc871fb88f2d9f54db176b4858f8366
Le <xingled@gmail.com>
2020-07-18 12:40:12 +0800

    Update install-mac.sh

--- vim/nvim/install-mac.sh
+++ vim/nvim/install-mac.sh
@@ -14,7 +14,7 @@ main() {
 
     # vim-plug
     if [ ! -f "~/.local/share/nvim/site/autoload/plug.vim" ]; then
-        curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+        curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs \
             https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
     fi
 
